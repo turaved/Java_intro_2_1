50ae88c7ea1be1afa159c5e4d4562f71323c455b
Alex Turovets <madlilu177@gmail.com>
2020-11-13 12:56:07 +0300

    Add program

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int CurrentBalance = 2_000_000_000;
+        int NewTransfer = 500_000_000;
+        int TotalBalance = CurrentBalance + NewTransfer;
+        System.out.println(TotalBalance);
+    }
+
+}
